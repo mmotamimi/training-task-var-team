2a1aefbffbb1ce8ac8c955815994d1561c708998
Mahmoud Shouli <mahmoud.shouli@foothillsolutions.com>
2025-09-29 01:48:27 +0300

    Create the landing page which routes to the three pages

---
+++ src/features/landing/LandingPage.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PageContainer from "../../shared/layout/PageContainer";
+import { IconButton } from "@mui/material";
+import { Link } from "@tanstack/react-router";
+import { TiWeatherPartlySunny } from "react-icons/ti";
+import { MdOutlineCurrencyExchange } from "react-icons/md";
+import { CiGlobe } from "react-icons/ci";
+import * as s from "../../shared/layout/pageContainer.styles";
+
+const LandingPage: React.FC = () => {
+  return (
+    <PageContainer>
+      <section className={s.hero}>
+        <div className={s.actions}>
+          <IconButton component={Link} to="/weather">
+            <TiWeatherPartlySunny />
+          </IconButton>
+          <IconButton component={Link} to="/currency">
+            <MdOutlineCurrencyExchange />
+          </IconButton>
+          <IconButton component={Link} to="/news">
+            <CiGlobe />
+          </IconButton>
+        </div>
+      </section>
+    </PageContainer>
+  );
+};
+
+export default LandingPage;
