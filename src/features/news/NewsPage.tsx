0a699dfcca453f821c28272b0731da800945517a
Mahmoud Shouli <mahmoud.shouli@foothillsolutions.com>
2025-09-29 01:49:00 +0300

    Create empty template

---
+++ src/features/news/NewsPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import PageContainer from "../../shared/layout/PageContainer";
+import { Typography } from "@mui/material";
+
+const NewsPage: React.FC = () => {
+  return (
+    <PageContainer menuEnabled>
+      <Typography variant="h5">News Template</Typography>
+    </PageContainer>
+  );
+};
+
+export default NewsPage;
