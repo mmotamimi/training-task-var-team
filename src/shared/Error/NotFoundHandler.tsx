a8836ac04d2a5e58d07986c8aa0f4922b8f1a2f5
Aya Ba’ara <aya.baara@foothillsolutions.com>
2025-09-30 11:29:22 +0300

    prevent ErrorDialog from opening twice

--- src/shared/Error/NotFoundHandler.tsx
+++ src/shared/Error/NotFoundHandler.tsx
@@ -1,12 +1,14 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useError } from "./context/ErrorContext";
 
 export default function NotFoundHandler() {
   const { error, setError } = useError();
+  const calledRef = useRef(false);
 
   useEffect(() => {
-    if (!error) {
+    if (!error && !calledRef.current) {
       setError("Page not found", 404);
+      calledRef.current = true;
     }
   }, [error, setError]);
 
