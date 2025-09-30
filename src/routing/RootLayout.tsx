571d56654fba98fe8f80d2ae6b9565cb3af99cdb
Aya Ba’ara <aya.baara@foothillsolutions.com>
2025-09-30 11:05:54 +0300

    integrate ErrorContext into RootLayout

--- src/routing/RootLayout.tsx
+++ src/routing/RootLayout.tsx
@@ -1,6 +1,32 @@
 import React from "react";
 import { Outlet } from "@tanstack/react-router";
+import { ErrorProvider, useError } from "../shared/Error/context/ErrorContext"
+import ErrorDialog from "../shared/Error/ErrorDialog";
 
-const RootLayout: React.FC = () => <Outlet />;
+const RootLayout = () => {
+  return (
+    <ErrorProvider>
+      <InnerLayout />
+    </ErrorProvider>
+  )
+}
 
+const InnerLayout = () => {
+  const { error, clearError } = useError();
+
+  return (
+    <div>
+      <Outlet />
+
+      {error && (
+        <ErrorDialog
+          open={!!error}
+          onClose={clearError}
+          message={error.message}
+          code={error.code}
+        />
+      )}
+    </div>
+  )
+}
 export default RootLayout;
